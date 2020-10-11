8c76080b16046107e7442bc2b71c4aebc0dc03de
atdykema <atdykema@gmail.com>
2020-10-11 18:30:17 -0400

    third commit, added more to get_hash_merkle_root but its still buggy. Need to fix later

--- block.py
+++ block.py
@@ -1,4 +1,5 @@
 from blockchainFiles.block_header import Block_header
+import hashlib
 
 
 class Block:
@@ -6,7 +7,7 @@ class Block:
         self.magic_no = "0xD9B4BEF9"
         self.block_height = block_height
         # Block_header(hash_prev_block, hash_merkle_root, target, nonce)
-        self.block_header = Block_header(hash_prev_block, "none, fix it", "none, fix it", "None, fix it")
+        self.block_header = Block_header(hash_prev_block, get_hash_merkle_root(input_transactions), "none, fix it", "None, fix it")
         self.num_of_transactions = len(input_transactions)
         self.input_transactions = input_transactions
 
@@ -17,8 +18,21 @@ def get_hash_merkle_root(transactions):
         return []
     temp = []
     while len(transactions) > 1:
-        if len(transactions) == 1:
-            transactions.append(transactions[0])
+        concat = transactions[len(transactions)-1] + transactions[len(transactions)-2]
+        curr_hash = hashlib.sha256(concat.encode('utf-8')).hexdigest()
+        temp.append(curr_hash)
+        del transactions[len(transactions) - 2: len(transactions) - 1]
+    if len(transactions) == 1:
+        transactions.append(transactions[0])
+        concat = transactions[0] + transactions[1]
+        curr_hash = hashlib.sha256(concat.encode('utf-8')).hexdigest()
+        temp.append(curr_hash)
+        del transactions[len(transactions) - 2: len(transactions) - 1]
+    if len(temp) > 1:
+        get_hash_merkle_root(temp)
+    else:
+        return temp
+
 
 
 
