ad6c326bef03f9f0f9b01c5fc238c90e31042cee
atdykema <atdykema@gmail.com>
2020-10-15 20:18:18 -0400

    set up testing for actual blockchain, moved everything from block_header to block as it was easier to deal with. merkle_tree no longer working for some reason

--- block.py
+++ block.py
@@ -1,21 +1,32 @@
-from blockchainFiles.block_header import Block_header
 import hashlib
+from random import random, seed, randrange
+import time
 
 
 class Block:
-    def __init__(self, block_height, hash_prev_block, input_transactions):
+    def __init__(self, next_block, prev_block, block_height, hash_prev_block, input_transactions):
+        self.block_hash = 123
+        self.next_block = next_block
+        self.prev_block = prev_block
         self.magic_no = "0xD9B4BEF9"
         self.block_height = block_height
         self.num_of_transactions = len(input_transactions)
         self.input_transactions = input_transactions
-        # Block_header(hash_prev_block, hash_merkle_root, target, nonce)
-        self.block_header = Block_header(hash_prev_block, get_hash_merkle_root(input_transactions), "none, fix it", "None, fix it")
+        # Blockhead------------------------------
+        self.block_version = get_version()
+        # TODO: hash_prev_block
+        self.hash_prev_block = hash_prev_block
+        self.hash_merkle_root = get_hash_merkle_root(input_transactions)
+        self.time_stamp = get_time_stamp()
+        # TODO: target
+        self.target = get_target(block_height, get_time_stamp())
+        # TODO: nonce
+        self.nonce = None
 
 
 def get_hash_merkle_root(transactions):
-    # TODO need sha256 hash
     tx = transactions.copy()
-    if tx == 0:
+    if len(tx) == 0:
         return []
     temp = []
     while len(tx) > 1:
@@ -35,6 +46,21 @@ def get_hash_merkle_root(transactions):
         return temp[0]
     else:
         get_hash_merkle_root(temp)
+
+
+def get_version():
+    versions = ['0x20400000', '0x20000000', '0x20c00000', '0x20e00000', '0x20800000', '0x3fffe000', '0x3fff0000',
+                '0x37ffe000', '0x20384000', '0x20600000',
+                '0x27ffe000']
+    return versions[randrange(0, 10, 1)]
+
+
+def get_time_stamp():
+    return int(time.time())
+
+
+def get_target(block_height, time_stamp):
+   return None
 
 
 
--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from blockchainFiles.hashing import create_hash
 from blockchainFiles.block import Block
 from random import randrange
 
+last_diff_change_block = None
+genesis_block = None
+global_block_height = None
+
+
+def find_block_at_height(desired_block_height, global_block_height, genesis_block):
+    block = genesis_block
+    while block.block_height < global_block_height:
+        if block.block_height == desired_block_height:
+            return block
+        else:
+            block = block.next_block
+    return genesis_block
+
 
 def create_random_transactions():
     num_of_transactions = randrange(1, 10, 1)
@@ -11,6 +25,18 @@ def create_random_transactions():
         transactions.append(create_hash())
         var += 1
     return transactions
+
+
+def create_test_blockchain():
+    first_block = Block(None, None, 0, "N/a", create_random_transactions())
+    blocks = 1
+    last_block = first_block
+    while blocks < 10:
+        temp = Block(None, last_block, blocks, last_block.hash_prev_block, create_random_transactions())
+        last_block.next_block = temp
+        last_block = temp
+        blocks += 1
+    return first_block
 
 
 def print_block(block):
@@ -39,38 +65,44 @@ def print_block(block):
 
     # version
     print("block version")
-    print(block.block_header.block_version)
+    print(block.block_version)
     print(".")
 
     # hash prev block
     print("previous hash block")
-    print(block.block_header.hash_prev_block)
+    print(block.hash_prev_block)
     print(".")
 
     # hash merkle tree
     print("merkle hash root")
-    print(block.block_header.hash_merkle_root)
+    print(block.hash_merkle_root)
     print(".")
 
     # hash prev block
     print("time stamp")
-    print(block.block_header.time_stamp)
+    print(block.time_stamp)
     print(".")
 
     # target
     print("target")
-    print(block.block_header.target)
+    print(block.target)
     print(".")
 
     # nonce
     print("nonce")
-    print(block.block_header.nonce)
+    print(block.nonce)
 
 
 def main():
-    # random_block = Block(block_height, hash_prev_block, input_transactions)
-    random_block = Block("none, fix it", 'None, fix it', create_random_transactions())
-    print_block(random_block)
+    curr_block = create_test_blockchain()
+    while curr_block.next_block is not None:
+        print_block(curr_block)
+        print("***")
+        print("***")
+        print("***")
+        print("***")
+        curr_block = curr_block.next_block
+    print_block(curr_block)
 
 
 if __name__ == '__main__':
